97401630333abdb64334cc0030965b81455f185a
Alex Archer <archeralex@gmail.com>
2021-10-01 14:20:41 +0100

    Add endpoint to allow POST of a new album

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ var albums = []album{
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
+    router.POST("/albums", postAlbums)
 
     router.Run("localhost:8080")
 }
@@ -31,4 +32,19 @@ func main() {
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+    var newAlbum album
+
+    // Call BindJSON to bind the received JSON to
+    // newAlbum.
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+
+    // Add the new album to the slice.
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
 }
